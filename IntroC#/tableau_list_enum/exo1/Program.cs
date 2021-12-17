47b8b46e5b1d4bf2de7622b0308813cf67491c4e
Khenjy DONAT <khenkyr@msn.com>
2021-12-17 11:47:25 +0100

    correction

--- IntroC#/tableau_list_enum/exo1/Program.cs
+++ IntroC#/tableau_list_enum/exo1/Program.cs
@@ -6,13 +6,18 @@ namespace exo1
     {
         static void Main(string[] args)
         {
-            string[] days = new string[] { "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"};
+            string[] days = new string[] { "Lundi", "Mardi", "Mercredi", "Jedi", "Vendredi", "Samedi"};
             Console.WriteLine("Indice 1: " + days[1]);
             Console.WriteLine("Indice 4: " + days[4]);
 
             Console.WriteLine("1er élément: " + days[0]);
             Console.WriteLine("4e élément: " + days[3]);
 
+            days[3] = "Jeudi";
+
+            Array.Resize(ref days, 7);
+
+            days[6] = "Dimanche";
 
             for(int i = 0; i < days.Length; i++)
             {
